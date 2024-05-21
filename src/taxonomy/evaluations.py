9867fe0c1fb18cf316844a4558f67913b50695dd
joshuaongg21 <joshuaongg21@gmail.com>
2024-05-21 22:26:40 +0100

    Add F1 Score

--- src/taxonomy/evaluations.py
+++ src/taxonomy/evaluations.py
@@ -2,11 +2,12 @@ import json
 import pandas as pd
 from tqdm import tqdm
 from src.taxonomy.data_utils import verbaliser
+from sklearn.metrics import f1_score
 
-
-def compute_metrics(pred_df):
-    exact_match = (pred_df["predicted_error_type"] == pred_df["error_type"]).mean()
-    return {"exact_match": exact_match}
+def compute_metrics(pred_df): 
+    exact_match = (pred_df["predicted_error_type"] == pred_df["error_type"]).mean() 
+    f1 = f1_score(pred_df["error_type"], pred_df["predicted_error_type"], average="weighted") 
+    return {"exact_match": exact_match, "f1_score": f1}
 
 def few_shot_prompt(examples, test_question, test_choices, test_answer):
     example_prompts = []
